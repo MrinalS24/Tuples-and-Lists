5329fbeddce2914de778c7565d84b27335ad446a
MrinalS24 <connectwithmrinaal@gmail.com>
2025-04-19 12:00:26 +0200

    Add files via upload

---
+++ Lists and Tuples/revision.py
@@ -0,0 +1,35 @@
+fav_princesses = ["Elsa","Aurora","Belle","Tiana","Rapunzel","Jasmine","Moana","Mulan","Merida","Snow White","Cinder Ella","Pocahantas","Taylor Swift"]
+print(fav_princesses)
+
+print()
+
+for princesses in fav_princesses:
+    print("My favorite princess is ", princesses)
+
+print()
+
+print(fav_princesses[12])
+
+print()
+
+print(fav_princesses[-13])
+
+print()
+
+fav_princesses.append("Ariel")
+print(fav_princesses)
+
+print()
+
+fav_princesses.remove("Snow White")
+print(fav_princesses)
+
+print()
+
+length = len(fav_princesses)
+print(length)
+
+print()
+
+print(fav_princesses[0:9])
+print(fav_princesses[-2:-8])
